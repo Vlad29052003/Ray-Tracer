a9864a5bd123ca4cdf35a278dc0075482035fb59
Vlad Graure <vladgraure@yahoo.ca>
2023-10-21 02:05:39 +0200

    implemented depth of field
    Former-commit-id: 99f8ab4c6f205ed0ae152f8fb8fd39f76bdeb44e [formerly 589151f6936aee41364f9c89a5c1a43b1e033919]
    Former-commit-id: 81071dac5ac0ab8f6144cf538595c7bd626a5121

--- src/extra.cpp
+++ src/extra.cpp
@@ -18,6 +18,37 @@ void renderImageWithDepthOfField(const Scene& scene, const BVHInterface& bvh, co
     }
 
     // ...
+    std::default_random_engine generator;
+    std::uniform_real_distribution<float> distribution(-0.5f, 0.5f);
+
+
+#ifdef NDEBUG // Enable multi threading in Release mode
+#pragma omp parallel for schedule(guided)
+#endif
+    for (int y = 0; y < screen.resolution().y; y++) {
+        for (int x = 0; x < screen.resolution().x; x++) {
+            glm::vec3 L = glm::vec3(0);
+            for (int samp = 0; samp < features.extra.numDofSamples; samp++) {
+                float xLens = features.extra.aperture * distribution(generator);
+                float yLens = features.extra.aperture * distribution(generator);
+                glm::vec2 position = (glm::vec2(x, y) + 0.5f) / glm::vec2(screen.resolution()) * 2.f - 1.f;
+                Ray ray = camera.generateRay(position);
+                glm::vec3 focalPoint = ray.origin + features.extra.focusDistance * ray.direction;
+                ray.origin += glm::vec3(xLens, yLens, 0);
+                ray.direction = glm::normalize(focalPoint - ray.origin);
+                RenderState state = {
+                    .scene = scene,
+                    .features = features,
+                    .bvh = bvh,
+                    .sampler = { static_cast<uint32_t>(screen.resolution().y * x + y) }
+                };
+                L += renderRay(state, ray, 0);
+            }
+
+            L /= features.extra.numDofSamples;
+            screen.setPixel(x, y, L);
+        }
+    }
 }
 
 // TODO; Extra feature
