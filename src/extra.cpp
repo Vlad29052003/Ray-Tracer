3f8b01a57cb10d2bf9efd4c9d749aa1ae39b6063
arielpotolski <arielpotolskie@gmail.com>
2023-11-02 10:03:21 +0100

    Implement bloom effect (still has an error.)
    Former-commit-id: 56cbc0a67e84e6b225d50d6726825924273f7d93 [formerly 3a5bd350654b84ed853cbdf9895e37c0b121e6c5]
    Former-commit-id: f0ed1a4abe56ad831453c725a315970d8d5baef6

--- src/extra.cpp
+++ src/extra.cpp
@@ -34,13 +34,56 @@ void renderImageWithMotionBlur(const Scene& scene, const BVHInterface& bvh, cons
 
 }
 
-void getBrightAreas(Screen& image) {
-    for (auto y = 0; y < image.resolution().y; y++) {
-        for (auto x = 0; x < image.resolution().x; x++) {
-            auto color = image.pixels()[image.indexAt(x, y)];
+Screen& getBrightAreas(Screen& image) {
+    Screen output(image.resolution());
+    for (auto y = 0; y < output.resolution().y; y++) {
+        for (auto x = 0; x < output.resolution().x; x++) {
+            auto color = output.pixels()[output.indexAt(x, y)];
             auto perceived_luminance = 0.2126f * color.r + 0.7152f * color.g + 0.0722f * color.b; //https://en.wikipedia.org/wiki/Relative_luminance
 
-            if
+            if (perceived_luminance > 0.9f)
+                output.pixels()[output.indexAt(x, y)] = { 1.0f, 1.0f, 1.0f };
+            else
+                output.pixels()[output.indexAt(x, y)] = { 0.0f, 0.0f, 0.0f };
+        }
+    }
+
+    return output;
+}
+
+float boxFilter(Screen& image, int i, int j, int col, int filterSize)
+{
+    filterSize = std::max(1, filterSize);
+    float sum = 0;
+    // Average pixels in the box-filter region
+    for (int x = -filterSize; x < filterSize + 1; x++)
+        for (int y = -filterSize; y < filterSize + 1; y++)
+            sum += image.pixels()[image.indexAt(i + x, j + y)][col];
+    sum /= (2 * filterSize + 1) * (2 * filterSize + 1);
+    return sum;
+}
+
+void filterImage(Screen& image, int filterSize)
+{
+    // we create a result image
+    Screen result(image);
+    // and process every channel of every pixel independently
+    for (int i = 0; i < image.resolution().x; i++)
+        for (int j = 0; j < image.resolution().y; j++) // for each pixel
+            for (int col = 0; col < 3; col++) // for each color channel
+                result.pixels()[result.indexAt(i, j)][col] = boxFilter(image, i, j, col, filterSize);
+}
+
+void combineFilteredWithOriginalImage(const Scene& scene, const Trackball& camera, Screen& originalImage, Screen& filteredImage)
+{
+    for (int y = 0; y < originalImage.resolution().y; y++) {
+        for (int x = 0; x < originalImage.resolution().x; x++) {
+            auto filteredPixel = filteredImage.pixels()[filteredImage.indexAt(x, y)];
+            auto originalPixel = originalImage.pixels()[originalImage.indexAt(x, y)];
+
+            auto bloomPixel = glm::clamp(originalPixel + filteredPixel, 0.0f, 1.0f);
+
+            originalImage.pixels()[originalImage.indexAt(x, y)] = bloomPixel;
         }
     }
 }
@@ -52,12 +95,15 @@ void getBrightAreas(Screen& image) {
 void postprocessImageWithBloom(const Scene& scene, const Features& features, const Trackball& camera, Screen& image)
 {
     if (features.extra.enableBloomEffect) {
-        getBrightAreas(image);
+        Screen& highValues = getBrightAreas(image);
+
+        int filterSize = (image.resolution().x - 1) / 2;
+        filterImage(highValues, filterSize);  // Implementation of this helper function got from the class slides
+        combineFilteredWithOriginalImage(scene, camera, image, highValues);
     }
 
     // ...
 }
-
 
 // Extra feature
 // Given a camera ray (or reflected camera ray) and an intersection, evaluates the contribution of a set of
